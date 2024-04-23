0659d393fcf981afdda4145cc7ae4833bbc5143b
Nina B <ngbuscemi@gmail.com>
2024-04-22 23:44:58 -0400

    added template code for resume

---
+++ src/pages/Resume/Resume.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <h2>My Resume</h2>
+      <p>
+        Insert your resume content here. You can include your education, work experience,
+        skills, certifications, and any other relevant information.
+      </p>
+      <h3>Proficiencies</h3>
+      <ul>
+        <li>Proficiency 1</li>
+        <li>Proficiency 2</li>
+        <li>Proficiency 3</li>
+        {/* Add more proficiencies as needed */}
+      </ul>
+      <p>
+        Feel free to format your resume content and proficiencies according to your preference.
+        You can also use HTML elements or other React components to structure your content.
+      </p>
+    </div>
+  );
+}
+
+export default Resume;
