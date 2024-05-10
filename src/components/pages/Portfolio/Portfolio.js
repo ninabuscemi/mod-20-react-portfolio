66f63fcf34cf1ae729ffd1013a34a6291efc3ac8
Nina B <ngbuscemi@gmail.com>
2024-05-10 11:44:13 -0400

    added comments to project and work files

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Project from './Work';
+import Project from './Work'; // Imports the project component
 import projectImage from '../../../assets/images/03-javascript-homework-demo.png';
 import projectImage2 from '../../../assets/images/04-proj-1.png';
-import projectImage3 from '../../../assets/images/05-note-taker.png'; // Import the image
+import projectImage3 from '../../../assets/images/05-note-taker.png'; // Import the image files
 
-
+// Array of project objects
 
 const projects = [
   {
     id: 0,
     title: "Password Generator",
     tools: "HTML, CSS, Javascript",
-    image: projectImage,
+    image: projectImage, // Assinging the imported image to the project
     description: "A web application that generates random passwords.",
     repo: "https://github.com/ninabuscemi/module-3",
     live: "https://ninabuscemi.github.io/module-3/",
@@ -40,9 +40,11 @@ const projects = [
 // adding projects later
 ];
 
+// Functional component for the Portfolio section
 const Portfolio = () => {
   return (
    <div className='m-4'>
+         {/* Rendering the Project component and passing the projects array as a prop */}
             <Project projects={projects} />
         </div>
   );
