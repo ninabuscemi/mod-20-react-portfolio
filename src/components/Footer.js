9d311e177d5fa09189cc71b02aab71f9c39fe446
Nina B <ngbuscemi@gmail.com>
2024-05-09 23:48:39 -0400

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,4 @@
 import React from 'react';
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLinkedin,
-  faGithub,
-  faInstagram
-} from "@fortawesome/free-brands-svg-icons";
-
 function Footer() {
   return (
 <footer className='footer'></footer>
