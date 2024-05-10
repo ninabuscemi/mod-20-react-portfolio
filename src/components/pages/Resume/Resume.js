ac9bf6cc5072b30f6760f8074fbdc06ad837179b
Nina B <ngbuscemi@gmail.com>
2024-05-09 20:07:07 -0400

    edited resume styling

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import resumeFile from '../../../assets/images/files/ninaresume.pdf';
+import ResumeContent from './ResumeContent';
+import './resume.css';
 
 const Resume = () => {
   return (
-    <div>
-      <h2>My Resume</h2>
-
-      Download my resume: <button onClick={() => window.open(resumeFile, '_blank')} 
-      className="btn btn-primary"> Download Resume </button>
-      <p>
-      </p>
-      <h3>Proficiencies</h3>
-      <ul>
-    Front end:
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JAVASCRIPT</li>
-        <li>jQuery</li>
-</ul>
-<ul> Back end:
-        <li> Python</li>
-        <li> MYSQL</li>
-        <li>MongoDB</li>
-        <li>Express</li>
-        <li>React</li>
-        <li>Node</li>
-</ul>  
-
-<p>
-      </p>
+    <div className="resume-container">
+      <ResumeContent />
+      {/* Other components or content */}
     </div>
   );
-}
+};
 
 export default Resume;
---
+++ src/components/pages/Resume/ResumeContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import resumeFile from '../../../assets/images/files/ninaresume.pdf';
+
+const Resume = () => {
+  return (
+    <div>
+      <h2>My Resume</h2>
+
+      Download my resume: <button onClick={() => window.open(resumeFile, '_blank')} 
+      className="btn btn-primary"> Download Resume </button>
+      <p>
+      </p>
+      <h3>Proficiencies</h3>
+      <ul>
+    Front end:
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JAVASCRIPT</li>
+        <li>jQuery</li>
+</ul>
+<ul> Back end:
+        <li> Python</li>
+        <li> MYSQL</li>
+        <li>MongoDB</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Node</li>
+</ul>  
+
+<p>
+      </p>
+    </div>
+  );
+}
+
+export default Resume;
