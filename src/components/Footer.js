f7459d391cdad89ca2b879c52f8be87d37d9b18c
Nina B <ngbuscemi@gmail.com>
2024-05-09 23:33:18 -0400

    added social media links to bio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,7 @@ import {
 
 function Footer() {
   return (
-    <footer className="footer">
-      
-      <div className='foot-social-media'>
-        <a href="https://github.com/ninabuscemi" className="github social mobile-foot"><i><FontAwesomeIcon icon={faGithub} size="2x" /></i></a>
-        <a href="https://www.linkedin.com/in/ninabuscemi/" className="linkedin social mobile-foot"><i><FontAwesomeIcon icon={faLinkedin} size="2x" /></i></a>
-        <a href="https://www.instagram.com/nina_coding/" className="insta social mobile-foot"><i><FontAwesomeIcon className='insta-icon' icon={faInstagram} size="2x" /></i></a>
-      </div>
-    </footer>
+<footer className='footer'></footer>
   );
 }
 
--- src/components/pages/AboutPage/AboutMe.js
+++ src/components/pages/AboutPage/AboutMe.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import './about.css';
 import portfolioPic from './portfolio_pic.jpg'; // Import the image file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function About() {
     return (
@@ -25,6 +27,18 @@ function About() {
                             Rooted in empathy and attention to detail, my approach ensures every project meets user needs. Inspired by my early experiences in Photoshop and HTML/CSS, I'm dedicated to crafting intuitive interfaces that make a difference.
                             </p>
                             <p>Looking forward, I'm committed to staying ahead in technology and delivering impactful solutions that resonate globally. </p>
+                        </div>
+
+                        <div className="social-links">
+                        <a href="https://github.com/ninabuscemi" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithub} size="2x" className="social-icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/ninabuscemi/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} size="2x" className="social-icon" />
+                        </a>
+                        <a href="https://www.instagram.com/nina_coding/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} size="2x" className="social-icon" />
+                        </a>
                         </div>
                         
                     </Col>
