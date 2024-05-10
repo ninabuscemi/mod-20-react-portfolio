a0c2f5b041a932df84b8af62907c5a9c8ab2b172
Nina B <ngbuscemi@gmail.com>
2024-05-09 23:24:39 -0400

    added links to github, insta, and linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Footer = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
   return (
-    <footer>
-      {/* Your footer content goes here */}
+    <footer className="footer">
+      
+      <div className='foot-social-media'>
+        <a href="https://github.com/ninabuscemi" className="github social mobile-foot"><i><FontAwesomeIcon icon={faGithub} size="2x" /></i></a>
+        <a href="https://www.linkedin.com/in/ninabuscemi/" className="linkedin social mobile-foot"><i><FontAwesomeIcon icon={faLinkedin} size="2x" /></i></a>
+        <a href="https://www.instagram.com/nina_coding/" className="insta social mobile-foot"><i><FontAwesomeIcon className='insta-icon' icon={faInstagram} size="2x" /></i></a>
+      </div>
     </footer>
   );
 }
