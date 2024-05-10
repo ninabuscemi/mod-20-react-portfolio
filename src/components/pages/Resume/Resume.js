d6004b0561de554bcd74aa47be7c1c6c8c33031a
Nina B <ngbuscemi@gmail.com>
2024-05-10 11:49:10 -0400

    added comments for resume files

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,9 +1,14 @@
 import React from 'react';
+
+// Importing the ResumeContent component
 import ResumeContent from './ResumeContent';
 import './resume.css';
 
+// Functional component for the Resume section
 const Resume = () => {
   return (
+
+   // Container div for the Resume section
     <div className="resume-container">
       <ResumeContent />
       {/* Other components or content */}
@@ -11,4 +16,5 @@ const Resume = () => {
   );
 };
 
+// Exporting the Resume component
 export default Resume;
--- src/components/pages/Resume/ResumeContent.js
+++ src/components/pages/Resume/ResumeContent.js
@@ -1,34 +1,40 @@
 import React from 'react';
+
+// Imports the resumem file
 import resumeFile from '../../../assets/images/files/ninaresume.pdf';
 
+// Function component for the Resume section
 const Resume = () => {
   return (
     <div>
+    {/*Header for the resume section*/}
       <h2>My Resume</h2>
 
+    {/* Button to download the resume file */}
       <button onClick={() => window.open(resumeFile, '_blank')} 
       className="btn btn-primary"> Download Resume </button>
-      <p>
-      </p>
+      <p> </p>
+
+    {/* Heading for the Proficiencies section */}
       <h3>Proficiencies</h3>
-      <ul>
-    Front end:
+    
+    {/* List of front-end proficiencies */}
+      <ul> Front end:
         <li>HTML</li>
         <li>CSS</li>
         <li>JAVASCRIPT</li>
         <li>jQuery</li>
-</ul>
-<ul> Back end:
+      </ul>
+    
+    {/* List of back-end proficiencies */}
+      <ul> Back end:
         <li> Python</li>
         <li> MYSQL</li>
         <li>MongoDB</li>
         <li>Express</li>
         <li>React</li>
         <li>Node</li>
-</ul>  
-
-<p>
-      </p>
+      </ul>   
     </div>
   );
 }
